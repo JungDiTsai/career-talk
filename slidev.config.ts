a375364f5434e5da3858ba5a3f04f1ed0edb96ca
JUNG-DI TSAI <chung.di.cai@gmail.com>
2026-05-13 10:03:20 +0800

    Fix Pages image 404 and use hash routing

--- slidev.config.ts
+++ slidev.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from '@slidev/cli'
 
 export default defineConfig({
   base: '/career-talk/',
+  routerMode: 'hash',
   exportFilename: 'career-talk-slides',
   vite: {
     base: '/career-talk/',
